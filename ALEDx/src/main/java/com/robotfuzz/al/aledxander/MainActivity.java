8477b0faede4d90d0b1a19211f43b0dc8c3de7da
Alistair Buxton <a.j.buxton@gmail.com>
2018-05-20 22:02:42 +0100

    Display a toast after permission request.

--- ALEDx/src/main/java/com/robotfuzz/al/aledxander/MainActivity.java
+++ ALEDx/src/main/java/com/robotfuzz/al/aledxander/MainActivity.java
@@ -2,11 +2,13 @@ package com.robotfuzz.al.aledxander;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +27,20 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,
                  new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
 
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String permissions[],
+            @NonNull int[] grantResults) {
+
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(MainActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
         } else {
-            // Permission has already been granted
+            Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
         }
     }
 
